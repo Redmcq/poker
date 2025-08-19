3a9894945c5c3ac789f28470451f5b8bcc0d8ef4
Redmcq <redmcq17@gmail.com>
2025-08-19 17:45:26 -0500

    chore: tooling baseline (ESLint, Prettier, Husky, CI, editorconfig, gitattributes, env example)

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+﻿/* eslint-env node */
+module.exports = {
+  root: true,
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  env: { browser: true, es2021: true, node: true, jest: true },
+  plugins: ["react", "react-hooks"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "eslint:recommended",
+    "prettier"
+  ],
+  settings: { react: { version: "detect" } },
+  rules: {
+    "react/prop-types": "off"
+  }
+};
